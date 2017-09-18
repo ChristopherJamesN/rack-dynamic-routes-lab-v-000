80f8f841df56c3a89d08ea38717f0deb2165ff17
Chris Nady <nady.christopher@gmail.com>
2017-09-18 17:15:54 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,51 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    elsif req.path.match(/add/)
+      item_term = req.params["item"]
+      resp.write handle_add(item_term)
+    else
+      resp.write "The path was not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+
+  def handle_search(search_term)
+    if @@items.include?(search_term)
+      return "#{search_term} is one of our items"
+    else
+      return "Couldn't find #{search_term}"
+    end
+  end
+
+  def handle_add(item_term)
+    if @@items.include?(item_term)
+      @@cart.push(item_term)
+      return "added #{item_term}"
+    else
+      return "We don't have that item"
+    end
+  end
+
+  def self.items
+    @@items
+  end
+
+  def self.cart
+    @@cart
+  end
+
+end
